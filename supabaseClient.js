e238984c7550de3296f43c8b45d398b74dac6ec9
Phongphat -Bas <122138377+basnakhu1999@users.noreply.github.com>
2025-05-30 17:49:54 +0700

    Delete supabaseClient.js

--- supabaseClient.js
+++
@@ -1,7 +0,0 @@
-// supabaseClient.js
-const SUPABASE_URL = 'https://qizejcrlxqailqjrqkeo.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFpemVqY3JseHFhaWxxanJxa2VvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQwNzU2MjksImV4cCI6MjA0OTY1MTYyOX0.wN-5phYnElhU7IPQQ6B8jehoJGD89POzJjXMWg511cg';
-
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-
-export { supabase };
